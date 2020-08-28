e984f610bb6f3bd2a0f2218e6ad141d2cf377171
cosmegt <mail@cosmechavez.dev>
2020-08-28 13:18:58 -0700

    deleted jade, clean start

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -2,6 +2,6 @@ const socket = io('http://localhost:3000', { //TODO: Change this for production
   path: '/socket'
 });
 
-function broadcast(){
-    socket.broadcast
+function broadcastHello(){
+    socket.emit("hello", "world")
 }
--- routes/game.js
+++
@@ -1,6 +0,0 @@
-var express = require("express");
-var router = require("router");
-
-class Game{
-    
-}
