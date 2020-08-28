3d27397542e55ff53898ec6ae36e7b2d6ac01f2b
Debian <admin@ip-172-26-6-3.us-west-2.compute.internal>
2020-08-28 17:22:32 +0000

    Actual latests from server

---
+++ routes/deck.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+
+class Deck {
+    constructor(){
+        this.lenght = 52s;
+        this.cards = [
+            this.diamonds, this.spades, this.hearts, this.clubs
+        ];
+        this.diamonds = {
+            diamonds_1 : "diamonds_1",
+            diamonds_2: "diamonds_2", 
+            diamonds_3: "diamonds_3",
+            diamonds_4: "diamonds_4", 
+            diamonds_5: "diamonds_5", 
+            diamonds_6: "diamonds_6",
+            diamonds_7: "diamonds_7", 
+            diamonds_8: "diamonds_8", 
+            diamonds_9: "diamonds_9",
+            diamonds_10: "diamonds_10",
+            diamonds_j: "diamonds_j",
+            diamonds_q: "diamonds_q",
+            diamonds_k: "diamonds_k"
+        };
+        this.spades = [
+            
+        ]
+    }
+
+}
