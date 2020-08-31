29031d64733e8f1603fecae7cdc619fc4c7478d5
cosmegt <mail@cosmechavez.dev>
2020-08-30 17:15:20 -0700

    setup for production

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -13,7 +13,7 @@ function ready(callback){
 /**
      * Socket Configuration and Set up
      */
-socket = io("80.70.60.14:3000", { //TODO: Change this for production
+socket = io("https://sandbox.cosmechavez.dev", { //TODO: Change this for production
     path: '/socket'
 });
 
